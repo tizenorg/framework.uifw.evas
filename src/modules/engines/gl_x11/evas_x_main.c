b3f89f1985356e7da540f81c0aa0d58e841b88ea
Gwanglim Lee <gl77.lee@samsung.com>
2010-06-22 17:00:43 +0900

    resolve rotation bug when using GLES backend

--- src/modules/engines/gl_x11/evas_x_main.c
+++ src/modules/engines/gl_x11/evas_x_main.c
@@ -202,6 +202,7 @@ eng_window_new(Display *disp,
         printf("Error: eglMakeCurrent() fail.\n");
         printf("Error: error # was: 0x%x\n", eglGetError());
      }
+   _evas_gl_x11_window = gw;
 
    vendor = glGetString(GL_VENDOR);
    renderer = glGetString(GL_RENDERER);
@@ -395,6 +396,7 @@ void
 eng_window_free(Evas_GL_X11_Window *gw)
 {
    win_count--;
+   eng_window_use (gw);
    if (gw == _evas_gl_x11_window) _evas_gl_x11_window = NULL;
    if (gw->gl_context) evas_gl_common_context_free(gw->gl_context);
 #if defined (GLES_VARIETY_S3C6410) || defined (GLES_VARIETY_SGX)
@@ -403,10 +405,10 @@ eng_window_free(Evas_GL_X11_Window *gw)
    if (win_count == 0)
      {
         if (context) eglDestroyContext(gw->egl_disp, context);
-        eglMakeCurrent(gw->egl_disp, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
         eglTerminate(gw->egl_disp);
         context = EGL_NO_CONTEXT;
      }
+   eglMakeCurrent(gw->egl_disp, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
 #else
    if (gw->glxwin) glXDestroyWindow(gw->disp, gw->glxwin);
    if (win_count == 0)
