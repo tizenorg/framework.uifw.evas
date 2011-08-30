9f1b9bae129096313604c6ab32ddfaf1e6eb484d
Jaehwan Kim <jae.hwan.kim@samsung.com>
2011-08-30 15:03:58 +0900

    fix the lockup bug in case of korean

--- src/lib/engines/common/evas_font_query.c
+++ src/lib/engines/common/evas_font_query.c
@@ -98,12 +98,6 @@ evas_common_font_query_run_font_end_get(RGBA_Font *fn, RGBA_Font_Int **script_fi
         /* If the script font doesn't fit even one char, find a new font. */
         if (itr == text)
           {
-             /* We should do nothing. */
-          }
-        else if (itr == text)
-          {
-             /* If the script font doesn't fit even one char, find a new font. */
-
              /* If we can find a font, use it. Otherwise, find the first
               * char the run of chars that can't be rendered until the first
               * one that can. */
