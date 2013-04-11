48590d07b1598bd3911684fc55c29ba16d1c9eb9
WooHyun Jung <wh0705.jung@samsung.com>
2013-04-11 14:46:17 +0900

    [Evas Font] Diasble run-time embolding in some cases. If OS/2 table is available and the font is demi-bold, don't do runtime imboldment.
    Change-Id: I09636d8c8a1a7adb0cd462840d08d2040dd2ea30

--- src/lib/engines/common/evas_font_load.c
+++ src/lib/engines/common/evas_font_load.c
@@ -5,6 +5,7 @@
 
 #include "evas_font_private.h" /* for Frame-Queuing support */
 #include "evas_font_ot.h"
+#include <freetype/tttables.h>
 
 #ifdef EVAS_CSERVE2
 # include "../../cserve2/evas_cs2_private.h"
@@ -495,7 +496,13 @@ evas_common_font_int_load_complete(RGBA_Font_Int *fi)
 
    if ((fi->wanted_rend & FONT_REND_WEIGHT) &&
        !(fi->src->ft.face->style_flags & FT_STYLE_FLAG_BOLD))
-      fi->runtime_rend |= FONT_REND_WEIGHT;
+     {
+        TT_OS2 *tt_os2 = FT_Get_Sfnt_Table(fi->src->ft.face, ft_sfnt_os2);
+        if (!tt_os2 || (tt_os2->usWeightClass < 600))
+          {
+             fi->runtime_rend |= FONT_REND_WEIGHT;
+          }
+     }
 
    return fi;
 }
