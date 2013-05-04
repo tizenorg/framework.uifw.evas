e8f258e0913d8b824706ee010ce45b663375b2ac
Jiyoun Park <jy0703.park@samsung.com>
2013-05-04 23:47:16 +0900

    fix PLM:P130424-3366 , agif file have normal frames and error frames we should show normal frames.
    Change-Id: Ie11a548ec49ea5d0a41802972ff60c181abbf956

--- src/modules/loaders/gif/evas_image_load_gif.c
+++ src/modules/loaders/gif/evas_image_load_gif.c
@@ -8,6 +8,10 @@
 
 #include <gif_lib.h>
 
+#ifndef MIN
+# define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
 typedef struct _Gif_Frame Gif_Frame;
 
 typedef enum _Frame_Load_Type
@@ -689,6 +693,10 @@ evas_image_load_file_head_gif(Image_Entry *ie, const char *file, const char *key
    int                 h;
    int                 alpha;
    int                 loop_count = -1;
+   Eina_Bool      animated = EINA_FALSE;
+   //it is possible which gif file have error midle of frames,
+   //in that case we should play gif file until meet error frame.
+   int            image_count = 0;
 
    w = 0;
    h = 0;
@@ -741,6 +749,7 @@ evas_image_load_file_head_gif(Image_Entry *ie, const char *file, const char *key
         if (DGifGetRecordType(gif, &rec) == GIF_ERROR)
           {
              /* PrintGifError(); */
+             if (image_count > 1) break; //we should show normal frames.
              DGifCloseFile(gif);
              *error = EVAS_LOAD_ERROR_UNKNOWN_FORMAT;
              return EINA_FALSE;
@@ -772,6 +781,7 @@ evas_image_load_file_head_gif(Image_Entry *ie, const char *file, const char *key
                   img = NULL;
                   DGifGetCodeNext(gif, &img);
                }
+             image_count++;
           }
         else if (rec == EXTENSION_RECORD_TYPE)
           {
@@ -810,12 +820,14 @@ evas_image_load_file_head_gif(Image_Entry *ie, const char *file, const char *key
 
    if (alpha >= 0) ie->flags.alpha = 1;
 
-   if (gif->ImageCount > 1)
+   if ((gif->ImageCount > 1) || (image_count > 1))
+     animated = EINA_TRUE;
+   if (animated)
      {
         ie->flags.animated = 1;
         ie->loop_count = loop_count;
         ie->loop_hint = EVAS_IMAGE_ANIMATED_HINT_LOOP;
-        ie->frame_count = gif->ImageCount;
+        ie->frame_count = MIN(gif->ImageCount, image_count);
         ie->frames = NULL;
      }
 
