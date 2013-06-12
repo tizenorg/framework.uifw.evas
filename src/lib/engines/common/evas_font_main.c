4a1b1dcf213196677b5efe3dbeb9f4ef7df77280
Sungho Kwak <sungho1.kwak@samsung.com>
2013-06-12 19:55:45 +0900

    Revert "[textblock] fix slowdown as a result of fixing text ascent/descent for fontsets from tom."
    This reverts commit f8946433384849743f6d3dbf4cae66ffa8f7ab5e.
    
    Change-Id: I2ec9b03aa5a85664fa3d7af88e06b8f977cc8f5e

--- src/lib/engines/common/evas_font_main.c
+++ src/lib/engines/common/evas_font_main.c
@@ -181,14 +181,11 @@ evas_common_font_ascent_get(RGBA_Font *fn)
    Eina_List *l;
    RGBA_Font_Int *fi;
 
-   if (fn->have_ascent) return fn->ascent;
    EINA_LIST_FOREACH(fn->fonts, l, fi)
      {
         v = evas_common_font_instance_ascent_get(fi);
         if (v > max) max = v;
      }
-   fn->ascent = max;
-   fn->have_ascent = 1;
    return max;
 }
 
@@ -200,14 +197,11 @@ evas_common_font_descent_get(RGBA_Font *fn)
    Eina_List *l;
    RGBA_Font_Int *fi;
 
-   if (fn->have_descent) return fn->descent;
    EINA_LIST_FOREACH(fn->fonts, l, fi)
      {
         v = evas_common_font_instance_descent_get(fi);
         if (v > max) max = v;
      }
-   fn->descent = max;
-   fn->have_descent = 1;
    return max;
 }
 
@@ -219,14 +213,11 @@ evas_common_font_max_ascent_get(RGBA_Font *fn)
    Eina_List *l;
    RGBA_Font_Int *fi;
 
-   if (fn->have_max_ascent) return fn->max_ascent;
    EINA_LIST_FOREACH(fn->fonts, l, fi)
      {
         v = evas_common_font_instance_max_ascent_get(fi);
         if (v > max) max = v;
      }
-   fn->max_ascent = max;
-   fn->have_max_ascent = 1;
    return max;
 }
 
@@ -238,14 +229,11 @@ evas_common_font_max_descent_get(RGBA_Font *fn)
    Eina_List *l;
    RGBA_Font_Int *fi;
 
-   if (fn->have_max_descent) return fn->max_descent;
    EINA_LIST_FOREACH(fn->fonts, l, fi)
      {
         v = evas_common_font_instance_max_descent_get(fi);
         if (v > max) max = v;
      }
-   fn->max_descent = max;
-   fn->have_max_descent = 1;
    return max;
 }
 
